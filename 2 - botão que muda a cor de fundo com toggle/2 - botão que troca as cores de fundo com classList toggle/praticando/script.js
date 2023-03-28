fd66daa09ba13109e72b1caa89149b3da07b6bb0
thaynalp <112887006+thaynalp@users.noreply.github.com>
2023-03-27 21:38:41 -0300

    exercício 3
    alterar texto do botão com if/else
    e manter o tamanho fixo do botão

--- 2 - botão que muda a cor de fundo com toggle/2 - botão que troca as cores de fundo com classList toggle/praticando/script.js
+++ 2 - botão que muda a cor de fundo com toggle/2 - botão que troca as cores de fundo com classList toggle/praticando/script.js
@@ -2,26 +2,35 @@ const btn_verde = document.getElementById('btn-verde')
 const btn_amarelo = document.getElementById('btn-amarelo')
 const btn_vermelho = document.getElementById('btn-vermelho')
 const body = document.querySelector('body')
-// const p = document.querySelector('p')
-
-
+const caixa_verde = document.getElementById('caixa-verde')
+const caixa_amarela = document.getElementById('caixa-amarela')
+const caixa_vermelha = document.getElementById('caixa-vermelha')
 
 function ativarCor(){
 
     btn_verde.addEventListener('click',()=>{
         body.classList.toggle('verde')
+        //   caixa.setAttribute('class','caixaApareceu')
+        caixa_verde.classList.toggle('caixaVerdeApareceu')
+        
     })
 
     btn_amarelo.addEventListener('click',()=>{
         body.classList.toggle('amarelo')
+        caixa_amarela.classList.toggle('caixaAmarelaApareceu')
+
     })
 
     btn_vermelho.addEventListener('click',()=>{
         body.classList.toggle('vermelho')
+        caixa_vermelha.classList.toggle('caixaVermelhaApareceu')
     })
     
 }
 ativarCor()
+
+
+
 
 //TOGGLE - SIGNIFICA 'ALTERNAR'   
 //"documento, na lista de classes alterne para verde"
